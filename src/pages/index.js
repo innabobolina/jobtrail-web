2f34ffed4b0bacfda786a5ba13f24f6a4ba0bc98
Darren Chang <73253544+Darrenyibochang@users.noreply.github.com>
2021-08-17 12:36:18 -0700

    Testimonial section & Footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { ThemeProvider } from 'theme-ui';
-import theme from 'theme';
+import React from "react";
+import { ThemeProvider } from "theme-ui";
+import theme from "theme";
 
-import SEO from 'components/seo';
-
-
+import SEO from "components/seo";
+import TestimonialList from "components/testimonial/TestimonialList";
+import Footer from "../components/Footer";
 
 export default function IndexPage() {
   return (
-    <ThemeProvider theme={theme}>
-  
-          <SEO title="Jobtrail App" />
-   
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <SEO title="Jobtrail App" />
+      </ThemeProvider>
+
+      <TestimonialList />
+      <Footer />
+    </>
   );
 }
