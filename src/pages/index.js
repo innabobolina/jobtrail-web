30e0e705aaa8e7c5f3cb8d7401d976ba5867b11c
Albert Koshy <Albertkoshy@gmail.com>
2021-09-01 21:14:33 -0700

    finished pricing section, it looks kind of weird but that's cause I followed the tutorial and I think we need the rest of the code it to make it look normal

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,22 @@ import { ThemeProvider } from 'theme-ui';
 import theme from 'theme';
 
 import SEO from 'components/seo';
+import Package from 'components/pricingSectionData';
 
 
 
 export default function IndexPage() {
   return (
     <ThemeProvider theme={theme}>
-  
           <SEO title="Jobtrail App" />
-   
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <Package /> 
+          <div />
     </ThemeProvider>
   );
 }
