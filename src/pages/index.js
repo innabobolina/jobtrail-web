6de1bb7af4956859b83d63278e5c74758ab94379
Zyanvel <zyanvel@gmail.com>
2021-09-15 17:35:35 -0700

    created video-feature component and reorganized components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,22 @@ import { ThemeProvider } from 'theme-ui';
 import theme from 'theme';
 
 // import SEO from 'components/seo';
-import Package from 'sections/pricingSectionData';
-import Feature from 'sections/feature';
-
-
 import Layout from "components/layout";
 import KeyFeature from "../sections/key-feature";
+import VideoFeature from 'components/feature-video';
+import Feature from 'sections/feature';
+import Package from 'sections/pricingSectionData';
 import TestimonialCard from "../sections/testimonial";
+
 
 export default function IndexPage() {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
+        <KeyFeature />
+        <VideoFeature/>
         <Feature />
         <Package /> 
-        <KeyFeature />
         <TestimonialCard />
       </Layout>
     </ThemeProvider>
